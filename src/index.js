f852762826734478ac12eae9d6909ee900df04e8
Alex Chikun <Claster85@gmail.com>
2020-10-03 19:09:52 +0300

    fix: change function if no params passed, or array is empty

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 
 exports.min = function min (array) {
-  let numMin = array.sort((a, b) => a - b) ;
-  return numMin[0];
+    if ((array.length !== 0) & (array !== '')) {
+        let numMin = array.sort((a, b) => a - b) ;
+        return numMin[0]
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-    let numMax = array.sort((a, b) => b - a) ;
-    return numMax[0];
+    if ((array.length !== 0) & (array !== '')) {
+        let numMax = array.sort((a, b) => b - a) ;
+        return numMax[0];
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-    let countAvg = array.reduce((acc , elem) => acc + elem) / array.length;
-  return countAvg;
+    if ((array.length !== 0) & (array !== '')) {
+        let countAvg = array.reduce((acc , elem) => acc + elem) / array.length;
+        return countAvg;
+    } else {
+        return 0;
+    }
 }
