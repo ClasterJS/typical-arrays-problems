c2558e01a1ba293c85a5e1eacae71965bed8d348
Alex Chikun <Claster85@gmail.com>
2020-10-03 18:48:31 +0300

    fix: rename array in functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 exports.min = function min (array) {
-  let numMin = a.sort((a, b) => a - b) ;
+  let numMin = array.sort((a, b) => a - b) ;
   return numMin[0];
 }
 
 exports.max = function max (array) {
-    let numMax = a.sort((a, b) => b - a) ;
+    let numMax = array.sort((a, b) => b - a) ;
     return numMax[0];
 }
 
 exports.avg = function avg (array) {
-    let countAvg = a.reduce((acc , elem) => acc + elem) / a.length;
+    let countAvg = array.reduce((acc , elem) => acc + elem) / array.length;
   return countAvg;
 }
